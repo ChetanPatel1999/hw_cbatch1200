aca8c440a2b31d43fd5a06ea7d7df957258462a4
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-03-05 13:10:54 +0530

    03/05

---
+++ 13_square.c
@@ -0,0 +1,10 @@
+//wap to printf square of given number.
+#include<stdio.h>
+void main()
+{
+  int num,sq;
+  printf("enter a num : ");
+  scanf("%d",&num); //5
+  sq=num*num;
+  printf("square of %d = %d",num,sq);	
+}
