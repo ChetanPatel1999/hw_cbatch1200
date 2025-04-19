19ecc2ffd518bf493aa36c2f3d5d93db0bc78ba7
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-04-19 13:30:59 +0530

    04/19

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+for (i = 1; i <= 5; i++) // 1
+    {
+        for (s = 1; s < i; s++)
+        {
+            printf(" ");
+        }
+        for (j = i; j <= 5; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
